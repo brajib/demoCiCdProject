6a577c8f6eeed2cb76c51103e841859c35758947
brajib <brajib245@gmail.com>
2024-10-23 23:36:35 +0530

    commit2
    new changes

--- src/demoCiCdProject/demo1.java
+++ src/demoCiCdProject/demo1.java
@@ -4,7 +4,7 @@ public class demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hi this is rajib");
 		System.out.println("I am QA engg");
-		System.out.println("I am from bangalore");
+		
 	}
 
 }
