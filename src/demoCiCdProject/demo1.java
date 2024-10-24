b1bcfd8b1778911ecbd4666bd12aeb5067e25a41
brajib <brajib245@gmail.com>
2024-10-24 20:57:07 +0530

    new change

--- src/demoCiCdProject/demo1.java
+++ src/demoCiCdProject/demo1.java
@@ -4,6 +4,7 @@ public class demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hi this is rajib");
 		System.out.println("I am QA engg");
+		System.out.println("I am from tripura");
 		
 	}
 
